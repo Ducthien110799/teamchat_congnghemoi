99d50f6ebb1938dd8ff6abe2cde954c70c8683cc
Ducthien110799 <nguyenthien110799@gmail.com>
2020-12-14 17:22:55 +0700

    thien sửa title

--- lang/vi.js
+++ lang/vi.js
@@ -1,7 +1,7 @@
 export const transValidation = {
-  email_incorrect: "Email phải có dạng example@trungqandev.com!",
+  email_incorrect: "Email phải có dạng example@gmail.com!",
   gender_incorrect: "Ủa, tại sao trường giới tính lại bị sai?",
-  password_incorrect: "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt.",
+  password_incorrect: "Mật khẩu phải chứa ít nhất 6 ký tự",
   password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác.",
   update_username: "Username giới hạn trong khoảng 3-17 kí tự và không được phép chứa kí tự đặc biệt.",
   update_gender: "Oops! Dữ liệu giới tính có vấn đề, bạn là hacker chăng?",
@@ -32,9 +32,9 @@ export const transErrors = {
 
 export const transSuccess = {
   userCreated: (userEmail) => {
-    return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email của bạn để active tài khoản trước khi đăng nhập. Xin cảm ơn!`;
+    return `Mã xác nhận đã được gửi tới <strong>${userEmail}</strong>`;
   },
-  account_actived: "Kích hoạt tài khoản thành công, bạn đã có thể đăng nhập vào ứng dụng.",
+  account_actived: "Kích hoạt tài khoản thành công, bạn đã có thể đăng nhập.",
   loginSuccess: (username) => {
     return `Xin chào ${username}, chúc bạn một ngày tốt lành.`;
   },
@@ -47,10 +47,11 @@ export const transMail = {
   subject: "Awesome Chat: Xác nhận kích hoạt tài khoản.",
   template: (linkVerify) => {
     return `
-      <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên ứng dụng Awesome Chat.</h2>
+      <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên Website Teamchat.</h2>
+      <h2>Cảm ơn bạn đã tin dùng ứng dụng của chúng tôi !</h2>
       <h3>Vui lòng click vào liên kết bên dưới để xác nhận kích hoạt tài khoản.</h3>
       <h3><a href="${linkVerify}" target="blank">${linkVerify}</a></h3>
-      <h4>Nếu tin rằng email này là nhầm lẫn, hãy bỏ qua nó. Trân trọng.</h4>
+      <h4>Xin trân thành cảm ơn</h4>
     `;
   },
   send_failed: "Có lỗi trong quá trình gửi email, vui lòng liên hệ lại với bộ phận hỗ trợ của chúng tôi."
